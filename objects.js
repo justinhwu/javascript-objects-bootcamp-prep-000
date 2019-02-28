ab95489d23a63a96d603c41e0ee485550e1d08b4
Justin Wu <justinhwu95@gmail.com>
2019-02-28 20:52:10 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = {artistName: "Excision"}
+var playlist = {artist: "Excision"}
 
 function updatePlaylist(playlist, artistName, songTitle){
   return Object.assign({}, playlist, {[artistName]: songTitle}) 
